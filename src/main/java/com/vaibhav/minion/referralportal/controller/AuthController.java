7b1a84b99ef5e181ac53fb069846f2a79b1b0a16
im-minion <vaibhavminiyar@gmail.com>
2019-05-28 23:04:22 +0530

    addded cross origin *

--- src/main/java/com/vaibhav/minion/referralportal/controller/AuthController.java
+++ src/main/java/com/vaibhav/minion/referralportal/controller/AuthController.java
@@ -6,13 +6,11 @@ import com.vaibhav.minion.referralportal.utility.LoginResponse;
 import com.vaibhav.minion.referralportal.utility.RegisterReposne;
 import com.vaibhav.minion.referralportal.utility.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/rp/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     @Autowired
--- src/main/java/com/vaibhav/minion/referralportal/controller/ReferralPortalController.java
+++ src/main/java/com/vaibhav/minion/referralportal/controller/ReferralPortalController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/rp")
+@CrossOrigin("*")
 public class ReferralPortalController {
 
     @Autowired
